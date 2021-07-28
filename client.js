c463d965555d57530e37635c3474dc4848ba21fe
Michael <michael.dburke56@gmail.com>
2021-07-28 13:45:09 -0400

    introduced movment

--- client.js
+++ client.js
@@ -9,6 +9,21 @@ conn.setEncoding("utf8");
 conn.on("data", (data) => {
   console.log(data)
 });
+
+conn.on("connect", () => {
+  console.log("Successfully connected to game server.")
+});
+
+//send message to server w/ .write()
+const name = 'Name: BRK'
+conn.write(`${name}`)
+
+//move
+setInterval(() => conn.write("Move: up"), 1000);
+setInterval(() => conn.write("Move: left"), 1500);
+
+
+
 return conn;
 };
 
